416404ca16758fa3cb34ebad6735131538929e4c
Paul Thomas <paulthomas2107@gmail.com>
2023-02-13 12:49:12 +0000

    More

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(response):
+    return HttpResponse("<h1>Hello, Paul !</h1>")
+
+
+def v1(response):
+    return HttpResponse("<h1>View 1 !</h1>")
